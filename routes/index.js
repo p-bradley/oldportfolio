b63304a2db9d992f05d5a746a10c29dc91e65ea1
p-bradley <p_bradley@fanshaweonline.ca>
2019-12-14 21:36:44 -0500

    added email options with nodemailer and finished tablet designs

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
 var sql = require('../utils/sql');
 
 router.get('/', function(req, res) {
@@ -49,6 +51,53 @@ router.get('/project/:id', (req, res) => {
 
 router.get('/contact', function(req, res) {
   res.render('contact', { title: 'Express' });
+});
+
+router.post('/send', (req, res) => {
+  console.log(req.body);
+  const output =` 
+  <p>New: Email</p>
+  <h3>Details</h3>
+  <ul>
+  <li>Name: ${req.body.name}</li>
+  <li>Email: ${req.body.email}</li>
+  <li>Subject: ${req.body.subject}</li>
+  </ul>
+  <h3>Message</h3>
+  <p>${req.body.message}</li>
+  `
+
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'pbradleyFanshaweC@gmail.com',
+        pass: 'portfolio'  
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  let mailOptions = {
+      from: '"New Email" pbradleyFanshaweC@gmail.com', 
+      to: 'wpatrickbradley99@gmail.com',
+      subject: 'Notification',
+      text: 'Hello world?',
+      html: output
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('contact', {message:'Thank you for contacting me, I will reply shortly!'});
+  });
+
 });
 
 module.exports = router;
